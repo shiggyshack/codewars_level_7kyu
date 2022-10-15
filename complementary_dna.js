9f07efa9d4bdfa68718b116fc7427df5460d0476
Sikes <99853518+shiggyshack@users.noreply.github.com>
2022-10-15 17:02:15 -0400

    Add files via upload

---
+++ complementary_dna.js
@@ -0,0 +1,34 @@
+// DESCRIPTION:
+
+// Deoxyribonucleic acid (DNA) is a chemical found in the nucleus of cells and carries the "instructions" for the development and functioning of living organisms.
+
+// If you want to know more: http://en.wikipedia.org/wiki/DNA
+
+// In DNA strings, symbols "A" and "T" are complements of each other, as "C" and "G". Your function receives one side of the DNA (string, except for Haskell); you need to return the other complementary side. DNA strand is never empty or there is no DNA at all (again, except for Haskell).
+
+// More similar exercise are found here: http://rosalind.info/problems/list-view/ (source)
+
+// Example: (input --> output)
+
+// "ATTGC" --> "TAACG"
+// "GTAT" --> "CATA"
+
+function DNAStrand(dna){
+    let complimentDNA = ''
+    for (let i = 0; i < dna.length; i++)
+    {
+      if(dna.charAt(i) == "A"){
+        complimentDNA += "T"
+      }
+      else if (dna.charAt(i) == "T"){
+        complimentDNA += "A"
+      }
+      else if (dna.charAt(i) == "C"){
+        complimentDNA += "G"
+      }
+      else{
+        complimentDNA += "C"
+      }
+    }
+    return complimentDNA
+  }
