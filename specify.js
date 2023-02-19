e4453ffcb6124e29c4565ae0a19109a09dfdf72d
Sikes <99853518+shiggyshack@users.noreply.github.com>
2023-02-19 09:49:23 -0500

    Create specify.js

---
+++ specify.js
@@ -0,0 +1,5 @@
+//Create a function that takes a string and adds space between each character as well as double the existing space.
+function spacify(str) {
+  //Use functions .split and .join to separate and then combine individual characters together with an additional space.
+  return str.split('').join(' ');
+}
