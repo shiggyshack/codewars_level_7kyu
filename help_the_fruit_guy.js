6b2fdf938148e1e4279b563eb720c3279c3f52b1
Sikes <99853518+shiggyshack@users.noreply.github.com>
2023-03-15 11:32:40 -0400

    Update help_the_fruit_guy.js

--- help_the_fruit_guy.js
+++ help_the_fruit_guy.js
@@ -1,18 +1,18 @@
-//Create a function, that accepts an array of strings representing fruit, if the name of the fruit has the word "rotten" attached to the front, update the element in the array to stay the name of the fruit with the word "rotten" removed.
-function removeRotten(bagOfFruits){
-  //If the array is empty or null, return an empty array.
-  if(bagOfFruits === [] || bagOfFruits === null){
-    return []
-  }
-  else{
-    //Iterate through each string contained in the array
-    for(let i = 0; i < bagOfFruits.length; i++){
-      //If the element contains the string 'rotten', then update the element using a .slice function starting after the 'rotten' string, leaving just the name of the fruit.
-      if(bagOfFruits[i].indexOf('rotten') !== -1){
-        bagOfFruits[i] = bagOfFruits.slice(5);
+//Create a function, that accepts a parameter of an array full of strings, where the strings are names of fruits or names of fruits with the word "rotten" in front of them. The function should replace any element with the word 'rotten' with only the name of the fruit.
+function removeRotten(bagOfFruits) {
+  //If the array is empty or null, retrun an empty array.
+  if (!bagOfFruits || bagOfFruits.length === 0) {
+    return [];
+  } else {
+    //If the array has elements, use a for loop to iterate through each element.
+    for (let i = 0; i < bagOfFruits.length; i++) {
+      //If the element contains 'rotten', update that element using .replace and .toLowerCase() methods
+      if (bagOfFruits[i].indexOf('rotten') !== -1) {
+        bagOfFruits[i] = bagOfFruits[i].replace('rotten', '').toLowerCase();
       }
     }
-    //Return the updated bagOfFruits array.
+    //Return the new array which will not contain any instance of the word 'rotten'
     return bagOfFruits;
   }
 }
+
